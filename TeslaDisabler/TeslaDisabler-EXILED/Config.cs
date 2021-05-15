bfbfcb41e62ef41138cd3562321bbe0b527922ab
Frezze <69349110+Unknown000Frezze@users.noreply.github.com>
2021-05-15 08:59:58 +0300

    TeslaDisabler

---
+++ TeslaDisabler/TeslaDisabler-EXILED/Config.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace TeslaDisabler
+{
+    public class Config : IConfig
+    {
+        [Description("Whether or not this plugin is enabled ")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Enable disabling tesla for Class-D Personal")]
+        public bool Classd { get; set; } = true;
+
+        [Description("Enable disabling tesla for Chaos Insurgency")]
+        public bool Chaos { get; set; } = true;
+
+        [Description("Enable disabling tesla for Cuffed Players")]
+        public bool Cuffed { get; set; } = true;
+
+        [Description("Enable disabling tesla for Scientists")]
+        public bool RSC { get; set; } = true;
+
+        [Description("Enable disabling tesla for Tutorials")]
+        public bool Tutorials { get; set; } = true;
+
+        [Description("Enable disabling tesla for Players with God Mode")]
+        public bool gm { get; set; } = true;
+
+        [Description("Enable disabling tesla for Mobile Task Force")]
+        public bool MTF { get; set; } = true; 
+
+
+    }
+}
